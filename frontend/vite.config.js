580197b5885220d173c1704dc1d590d0fab97975
Abhinav Choudhary <cabhinav2005@gmail.com>
2025-02-09 01:57:00 +0530

    vercel config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    historyApiFallback: true, // Fixes 404 on refresh
+    // Fixes 404 on refresh for SPA (Single Page Application)
+    historyFallback: true,
   },
-})
+});
